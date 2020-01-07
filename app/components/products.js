c3010a8809780a77e1cdc5e439a32d7505435485
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2020-01-07 22:35:59 +0500

    data tables update on new record creation

--- app/components/products.js
+++ app/components/products.js
@@ -1,18 +1,38 @@
 import Component from '@glimmer/component';
-import { set, get, action } from '@ember/object';
+import { computed, set, get, action } from '@ember/object';
 import { task } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
 import $ from 'jquery';
+import { tracked } from "@glimmer/tracking";
 
 export default class ProductsComponent extends Component {
     @service() store;
-    
+    @tracked allProducts = this.args.products;
+
     constructor() {
         super(...arguments);
         set(this, 'product', {});
 
         $(document).ready( function () {
             $('#table_id').DataTable();
+        } );
+    }
+
+    @computed('allProducts')
+    get productSelect() {
+
+        let dataSet = this.allProducts.map(p => [p.company, p.model, p.retail_price, p.stock
+        ]);
+        $("#table_id").dataTable().fnDestroy();
+        // return this.allProducts;
+        $('#table_id').DataTable( {
+            data: dataSet,
+            columns: [
+                { title: "Company" },
+                { title: "Model" },
+                { title: "Retail Price" },
+                { title: "Stock" }
+            ]
         } );
     }
 
